f025ed3c94a1044ef3fdaf531d6375ec1f5bb1df
juanosarg <31105173+juanosarg@users.noreply.github.com>
2023-03-29 16:36:44 +0200

    v0.1

--- 1.4/Source/AlphaMechs/AlphaMechs/Projectiles/Projectile_Distortion.cs
+++ 1.4/Source/AlphaMechs/AlphaMechs/Projectiles/Projectile_Distortion.cs
@@ -30,7 +30,7 @@ namespace AlphaMechs
                 catch (Exception) { }
 
             }
-            if (this.IsHashIntervalTick(50))
+            if (this.IsHashIntervalTick(25))
             {
                 var target = NextTarget();
                 if (target != null)
@@ -45,7 +45,7 @@ namespace AlphaMechs
         private Thing NextTarget()
         {
             var things = GenRadial.RadialDistinctThingsAround(this.PositionHeld, Map, 15, false)
-                .Where(t => (t.HostileTo(this.launcher)));
+                .Where(t => (t.Faction!=this.launcher.Faction && t as Pawn!=null));
             
             things = things.OrderBy(t => t.Position.DistanceTo(this.Position));
             var target = things.FirstOrDefault();
@@ -55,7 +55,8 @@ namespace AlphaMechs
         public void FireAt(Thing target)
         {
             var projectile = (Projectile)GenSpawn.Spawn(InternalDefOf.AM_Bullet_Starfire_Secondary, Position, Map);
-            projectile.Launch(launcher, target, target, this.HitFlags, false);
+            
+            projectile.Launch(launcher, target, target, this.HitFlags, false,launcher);
           
         }
 
