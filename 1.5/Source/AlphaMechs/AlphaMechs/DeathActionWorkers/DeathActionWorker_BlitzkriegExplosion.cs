8531bd10f01e03fa50a569e27bbe0c566c4b4b99
juanosarg <31105173+juanosarg@users.noreply.github.com>
2024-03-21 12:24:10 +0100

    v2.0

---
+++ 1.5/Source/AlphaMechs/AlphaMechs/DeathActionWorkers/DeathActionWorker_BlitzkriegExplosion.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI.Group;
+
+namespace AlphaMechs
+{
+	public class DeathActionWorker_BlitzkriegExplosion : DeathActionWorker
+	{
+		public override RulePackDef DeathRules => RulePackDefOf.Transition_DiedExplosive;
+
+		public override bool DangerousInMelee => true;
+
+		public override void PawnDied(Corpse corpse, Lord prevLord)
+		{
+			GenExplosion.DoExplosion(radius: 3.9f, center: corpse.Position, map: corpse.Map, damType: DamageDefOf.Bomb, damAmount:30,chanceToStartFire:0.5f,instigator: corpse.InnerPawn);
+		}
+	}
+}
---
+++ 1.5/Source/AlphaMechs/AlphaMechs/PlaceWorkers/PlaceWorker_GreaterBandNode.cs
@@ -0,0 +1,33 @@
+﻿
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+namespace AlphaMechs
+{
+    public class PlaceWorker_GreaterBandNode : PlaceWorker
+    {
+
+        List<ThingDef> listBandNodes = new List<ThingDef>() { ThingDefOf.BandNode,InternalDefOf.AM_BeamcasterBandNode, InternalDefOf.AM_GreaterBandNode};
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef def, IntVec3 center, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            foreach (IntVec3 item in GenAdj.OccupiedRect(center, rot, def.Size).ExpandedBy(1))
+            {
+                if (!item.InBounds(map))
+                {
+                    continue;
+                }
+                List<Thing> thingList = item.GetThingList(map);
+                for (int i = 0; i < thingList.Count; i++)
+                {
+                    if (listBandNodes.Contains(thingList[i].def) || listBandNodes.Contains(thingList[i].def.entityDefToBuild))
+                    {
+                        return "AM_MustNotBePlacedCloseToAnother".Translate();
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
