b504238f8505ffe9273705d2ba928a3e1f4b013b
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-06-24 16:12:54 +0200

    v3.0

---
+++ 1.6/Source/AlphaMechs/AlphaMechs/Abilities/CompResurrectMechMinor.cs
@@ -0,0 +1,111 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+namespace AlphaMechs
+{
+    public class CompResurrectMechMinor : CompAbilityEffect
+    {
+      
+       
+
+        public new CompProperties_ResurrectMechMinor Props => (CompProperties_ResurrectMechMinor)props;
+
+
+
+      
+        public override bool CanApplyOn(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            if (!base.CanApplyOn(target, dest))
+            {
+                return false;
+            }
+            Corpse corpse;
+            if (!target.HasThing || (corpse = target.Thing as Corpse) == null)
+            {
+                
+                return false;
+            }
+            if (!CanResurrect(corpse))
+            {
+               
+                return false;
+            }
+          
+            return true;
+        }
+
+       
+
+        private bool CanResurrect(Corpse corpse)
+        {
+            if (!corpse.InnerPawn.RaceProps.IsMechanoid || corpse.InnerPawn.RaceProps.mechWeightClass == MechWeightClassDefOf.UltraHeavy)
+            {
+                return false;
+            }
+            if (corpse.InnerPawn.Faction != parent.pawn.Faction)
+            {
+                return false;
+            }
+            if (corpse.InnerPawn.kindDef.abilities != null && corpse.InnerPawn.kindDef.abilities.Contains(AbilityDefOf.ResurrectionMech))
+            {
+                return false;
+            }
+           
+           
+            return true;
+        }
+
+
+
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            base.Apply(target, dest);
+            Corpse corpse = (Corpse)target.Thing;
+          
+            if (CanResurrect(corpse))
+            {
+                Pawn innerPawn = corpse.InnerPawn;
+               
+                ResurrectionUtility.TryResurrect(innerPawn);
+                if (Props.appliedEffecterDef != null)
+                {
+                    Effecter effecter = Props.appliedEffecterDef.SpawnAttached(innerPawn, innerPawn.MapHeld);
+                    effecter.Trigger(innerPawn, innerPawn);
+                    effecter.Cleanup();
+                }
+                innerPawn.stances.stagger.StaggerFor(60);
+            }
+        }
+
+       
+
+      
+
+        public override IEnumerable<Mote> CustomWarmupMotes(LocalTargetInfo target)
+        {
+            foreach (LocalTargetInfo affectedTarget in parent.GetAffectedTargets(target))
+            {
+                Thing thing = affectedTarget.Thing;
+                yield return MoteMaker.MakeAttachedOverlay(thing, ThingDefOf.Mote_MechResurrectWarmupOnTarget, Vector3.zero);
+            }
+        }
+
+        public override void PostApplied(List<LocalTargetInfo> targets, Map map)
+        {
+            Vector3 zero = Vector3.zero;
+            foreach (LocalTargetInfo target in targets)
+            {
+                zero += target.Cell.ToVector3Shifted();
+            }
+            zero /= (float)targets.Count();
+            IntVec3 intVec = zero.ToIntVec3();
+            EffecterDefOf.ApocrionAoeResolve.Spawn(intVec, map).EffectTick(new TargetInfo(intVec, map), new TargetInfo(intVec, map));
+        }
+
+       
+    }
+}
---
+++ 1.6/Source/AlphaMechs/AlphaMechs/Projectiles/Projectile_Distortion.cs
@@ -0,0 +1,73 @@
+﻿
+using Verse;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+using Verse.Sound;
+using UnityEngine;
+using System.Collections;
+using static HarmonyLib.Code;
+using System.Linq;
+
+namespace AlphaMechs
+{
+    public class Projectile_Distortion : Projectile_Explosive
+    {
+
+
+
+        protected override void Tick()
+        {
+            base.Tick();
+            if (this.IsHashIntervalTick(10))
+            {
+                try
+                {
+                    FleckMaker.AttachedOverlay(this, FleckDefOf.PsycastAreaEffect, Vector3.zero, 3f, -1f);
+                }
+                catch (Exception) { }
+
+            }
+            if (this.IsHashIntervalTick(25))
+            {
+                var target = NextTarget();
+                if (target != null)
+                {
+                    FireAt(target);
+                }
+            }
+            
+        }
+
+
+        private Thing NextTarget()
+        {
+            if (Map != null && this.PositionHeld.InBounds(Map))
+            {
+                var things = GenRadial.RadialDistinctThingsAround(this.PositionHeld, Map, 15, false)
+                .Where(t => (t.Faction != this.launcher?.Faction && t as Pawn != null));
+
+                things = things.OrderBy(t => t.Position.DistanceTo(this.Position));
+                var target = things.FirstOrDefault();
+                return target;
+            }
+            else return null;
+            
+        }
+
+        public void FireAt(Thing target)
+        {
+            var projectile = (Projectile)GenSpawn.Spawn(InternalDefOf.AM_Bullet_Starfire_Secondary, Position, Map);
+            
+            projectile.Launch(launcher, target, target, this.HitFlags, false,launcher);
+          
+        }
+
+
+
+
+
+    }
+}
